26729407d2ed44168c3fef6a3e95e861b8ca745d
Mateusz Tomczak <99652987+tomczak-mateusz@users.noreply.github.com>
2023-02-12 11:48:08 +0100

    [11] update 'has a version number' spec to the one equal to the current value (#10)

--- spec/cosmopolitan/nip_spec.rb
+++ spec/cosmopolitan/nip_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Cosmopolitan::Nip do
-  it "has a version number" do
-    expect(Cosmopolitan::Nip::VERSION).not_to be nil
+  it "version is equal to 0.1.0" do
+    expect(Cosmopolitan::Nip::VERSION).to eq("0.1.0")
   end
 end
