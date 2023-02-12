6d487f49c5402e82c587275cee67a93c99058e11
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-02-12 09:51:40 +0100

    [3] remove initial failing spec - 'does something useful'

--- spec/cosmopolitan/nip_spec.rb
+++ spec/cosmopolitan/nip_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Cosmopolitan::Nip do
   it "has a version number" do
     expect(Cosmopolitan::Nip::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
