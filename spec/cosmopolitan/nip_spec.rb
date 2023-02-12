87a7fd52802de1a2779733d76355caa0b4f96400
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-02-12 11:54:39 +0100

    [13] update every double quotes to single quotes

--- spec/cosmopolitan/nip_spec.rb
+++ spec/cosmopolitan/nip_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Cosmopolitan::Nip do
-  it "version is equal to 0.1.0" do
-    expect(Cosmopolitan::Nip::VERSION).to eq("0.1.0")
+  it 'version is equal to 0.1.0' do
+    expect(Cosmopolitan::Nip::VERSION).to eq('0.1.0')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "cosmopolitan/nip"
+require 'cosmopolitan/nip'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Random spec order
   config.order = :random
