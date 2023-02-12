73e355da1de8ee3ed345cec97273261938d1ac9e
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-02-12 09:46:06 +0100

    [1] initialize Cosmopolitan::Nip gem - include Ruby 3.1.2, RSpec, GitHub Actions

---
+++ lib/cosmopolitan/nip.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "nip/version"
+
+module Cosmopolitan
+  module Nip
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cosmopolitan/nip/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Cosmopolitan
+  module Nip
+    VERSION = "0.1.0"
+  end
+end
